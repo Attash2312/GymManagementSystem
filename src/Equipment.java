74e506cdfae2b4e02d83f331ce57222509a03a24
Attash2312 <attash2312@gmail,com>
2023-11-26 18:39:56 +0500

    equipment class changes

--- src/Equipment.java
+++ src/Equipment.java
@@ -1,10 +1,14 @@
 public class Equipment {
     private String name;
     private int quantity;
+    private double price;
+    private String description;
 
     public Equipment(String name, int quantity) {
         this.name = name;
         this.quantity = quantity;
+        this.price = price;
+        this.description = description;
     }
 
     public String getName() {
